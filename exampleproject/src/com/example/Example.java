646945d8691d4963267529321f9d4093b279a1ee
satya.bandaru <satya.bandaru@N13304.intellectdesign.com>
2021-10-19 14:02:37 +0530

    My Example

---
+++ exampleproject/src/com/example/Example.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class Example {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Example [name=" + name + "]";
+	}
+	
+}
